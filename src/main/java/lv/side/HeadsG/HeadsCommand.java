dc2a615ff73916b1ea3ade5c7ede0bdd7b553cf8
paalmijs <palmijskruts@gmail.com>
2023-06-29 14:31:53 +0300

    Pievienots TabComplete un Config.yml fails.

--- src/main/java/lv/side/HeadsG/HeadsCommand.java
+++ src/main/java/lv/side/HeadsG/HeadsCommand.java
@@ -4,18 +4,21 @@ import me.arcaniax.hdb.api.HeadDatabaseAPI;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
-public class HeadsCommand implements CommandExecutor {
+public class HeadsCommand implements CommandExecutor, TabCompleter {
 
-    private final HeadsMain headDatabaseAPI;
+    private final HeadsMain headsMain;
     private final Random random;
 
-    public HeadsCommand(HeadsMain headDatabaseAPI) {
-        this.headDatabaseAPI = headDatabaseAPI;
+    public HeadsCommand(HeadsMain headsMain) {
+        this.headsMain = headsMain;
         this.random = new Random();
     }
 
@@ -28,28 +31,43 @@ public class HeadsCommand implements CommandExecutor {
 
         Player player = (Player) sender;
 
-
         if (args.length == 0) {
             HeadDatabaseAPI api = new HeadDatabaseAPI();
             ItemStack headItem = api.getRandomHead();
-            player.getInventory().addItem(headItem);
 
-            player.sendMessage("You have been given a random head!");
-        } else {
+            if (headItem != null) {
+                player.getInventory().addItem(headItem);
+                player.sendMessage("You have been given a random head!");
+            } else {
+                player.sendMessage("Failed to retrieve a random head.");
+            }
+        } else if (args.length == 1) {
             String argument = args[0];
 
-                HeadDatabaseAPI api = new HeadDatabaseAPI();
-                ItemStack headItem = api.getItemHead(argument);
-                if (headItem != null) {
-                    player.getInventory().addItem(headItem);
-                    player.sendMessage("You have been given the head with ID: " + argument);
-                } else {
-                    player.sendMessage("Failed to retrieve head with ID: " + argument);
-                }
+            HeadDatabaseAPI api = new HeadDatabaseAPI();
+            ItemStack headItem = api.getItemHead(argument);
 
-            return true;
+            if (headItem != null) {
+                player.getInventory().addItem(headItem);
+                player.sendMessage("You have been given the head with ID: " + argument);
+            } else {
+                player.sendMessage("Failed to retrieve head with ID: " + argument);
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 1) {
+            List<String> completions = new ArrayList<>();
+            List<String> headOptions = headsMain.getHeadOptions();
+
+            completions.addAll(headOptions);
+
+            return completions;
         }
 
-        return true;
+        return null;
     }
 }
--- src/main/java/lv/side/HeadsG/HeadsMain.java
+++ src/main/java/lv/side/HeadsG/HeadsMain.java
@@ -1,19 +1,47 @@
 package lv.side.HeadsG;
 
-
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.event.Listener;
 
+import java.io.File;
+import java.util.List;
 
-public class HeadsMain extends JavaPlugin implements Listener {
+public class HeadsMain extends JavaPlugin {
+    private List<String> headOptions;
+
     @Override
     public void onEnable() {
         getLogger().info("HeadsM has been enabled!");
-        getCommand("gethead").setExecutor(new HeadsCommand((this)));
+
+        loadConfigValues();
+
+        getCommand("gethead").setExecutor(new HeadsCommand(this));
+        getCommand("gethead").setTabCompleter(new HeadsCommand(this));
     }
+
     @Override
     public void onDisable() {
         getLogger().info("HeadsM has been disabled!");
     }
 
+    private void loadConfigValues() {
+
+        File dataFolder = getDataFolder();
+
+
+        File configFile = new File(dataFolder, "config.yml");
+        if (configFile.exists()) {
+
+            FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+            headOptions = config.getStringList("head-options");
+        } else {
+            getLogger().warning("Config file not found. Creating a new one.");
+            saveDefaultConfig();
+        }
+    }
+
+    public List<String> getHeadOptions() {
+        return headOptions;
+    }
 }
